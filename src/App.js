e51bd255cf8286e26acbe0ff414c1078c0b7d827
Jesse Eikema <jesse@voorhoede.nl>
2021-01-24 15:42:18 +0100

    project setup update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 
+import { SearchBar } from './components/searchBar/searchBar'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h2>Hello world</h2>
-        </div>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <>
+        <SearchBar label="Search" />
+      </>
     );
   }
 }
